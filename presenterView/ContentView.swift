48ffd0b8b0154f8f7c52bf186227b8a5f364aaa6
Ben Jones <ben.james.jones@gmail.com>
2021-10-01 16:13:29 -0600

    Add refresh button

--- presenterView/ContentView.swift
+++ presenterView/ContentView.swift
@@ -8,15 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var windowPreviews : [WindowPreview] = []
+    
+    
+    
     var body: some View {
         VStack {
             Text("Presenter View")
                 .font(.title)
                 .padding()
             
+            Text("Select Window to Share")
+                .font(.subheadline)
+                .padding()
+            
+            Button(action: refreshWindows) {
+                Text("Refresh Windows")
+                    .font(.subheadline)
+            }
+            
             ScrollView{
                 LazyVGrid(columns: Array(repeating: GridItem.init(.fixed(300)), count: 4)){
-                    ForEach(getWindowPreviews()) { windowPreview in
+                    ForEach(windowPreviews) { windowPreview in
                         VStack {
                             Image(windowPreview.image, scale: 1.0, orientation: Image.Orientation.up, label: Text(windowPreview.title))
                                 .resizable()
@@ -26,11 +39,17 @@ struct ContentView: View {
                             Text("\(windowPreview.owner): \(windowPreview.title)")
                         }
                     }
-                }
+                }.onAppear(perform: refreshWindows)
             }
         }
     }
+    
+    func refreshWindows() -> Void {
+        windowPreviews = getWindowPreviews()
+    }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
