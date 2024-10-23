957a0ba3060221bf05c83ac02aa8c0a0115eeb5b
Ben Jones <ben.james.jones@gmail.com>
2024-10-23 14:00:39 -0600

    add another test for conservative trigger

--- presenterModeTests/presenterModeTests.swift
+++ presenterModeTests/presenterModeTests.swift
@@ -12,20 +12,42 @@ struct presenterModeTests {
 
     @Test func CTSingleUpdate() {
         var trigger = ConservativeTrigger()
-        var result = trigger.updateUpcoming()
-        #expect(!result)
-        result = trigger.tick(updateOccurred: true)
-        #expect(!result)
-        #expect(trigger.updateUpcoming())
-        for _ in 1..<trigger.framesToWait {
-            let result = trigger.tick(updateOccurred: false)
+        for _ in 1...10 {
+            var result = trigger.updateUpcoming()
+            #expect(!result)
+            result = trigger.tick(updateOccurred: true)
             #expect(!result)
             #expect(trigger.updateUpcoming())
+            for _ in 1..<trigger.framesToWait {
+                let result = trigger.tick(updateOccurred: false)
+                #expect(!result)
+                #expect(trigger.updateUpcoming())
+            }
+            
+            result = trigger.tick(updateOccurred: false)
+            #expect(result)
         }
         
+    }
+
+    
+    @Test func CTMultipleUpdate() {
+        var trigger = ConservativeTrigger()
+        var result = trigger.updateUpcoming()
+        #expect(!result)
+        
+        for _ in 1...100 {
+            result = trigger.tick(updateOccurred: true)
+            #expect(!result)
+            #expect(trigger.updateUpcoming())
+            for _ in 1..<trigger.framesToWait {
+                let result = trigger.tick(updateOccurred: false)
+                #expect(!result)
+                #expect(trigger.updateUpcoming())
+            }
+        }
         result = trigger.tick(updateOccurred: false)
         #expect(result)
         
     }
-
 }
