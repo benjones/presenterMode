3329741dfd3044dd617541c7febd7876a6b0ba33
Ben Jones <ben.james.jones@gmail.com>
2024-10-23 11:35:09 -0600

    added first unit test.  Xcode sucks major ass to set this up for!

---
+++ presenterModeTests/presenterModeTests.swift
@@ -0,0 +1,16 @@
+//
+//  presenterModeTests.swift
+//  presenterModeTests
+//
+//  Created by Ben Jones on 10/23/24.
+//
+
+import Testing
+
+struct presenterModeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
