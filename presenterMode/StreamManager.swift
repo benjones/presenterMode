db804242988c74f633022feaffe82b125b498601
Ben Jones <ben.james.jones@gmail.com>
2026-06-01 16:40:13 -0600

    extract streamtoframes delegate

--- presenterMode/StreamManager.swift
+++ presenterMode/StreamManager.swift
@@ -27,6 +27,7 @@ let sharingStoppedImage: CGImage = CGImage(
     pngDataProviderSource: CGDataProvider(data: NSDataAsset(name: "sharingStopped")!.data as CFData)!,
     decode: nil, shouldInterpolate: true, intent: .defaultIntent)!
 
+
 class StreamManager: NSObject, ObservableObject, SCContentSharingPickerObserver {
     
     @Published var history = [HistoryEntry]()
@@ -238,220 +239,27 @@ class StreamManager: NSObject, ObservableObject, SCContentSharingPickerObserver 
     
     func getFrameSequence() -> AsyncThrowingStream<FrameType, Error> {
         return AsyncThrowingStream<FrameType, Error> { continuation in
-            self.scDelegate =
-            StreamToFramesDelegate(continuation: continuation,
-                                   recorder: avRecorder,
-                                   getCurrentFilter: { self.currentFilter}
-            ){
-                self.runningStream = nil
-            }
+            let callbacks = StreamFrameCallbacks(
+                onFrame: { frame in
+                    continuation.yield(frame)
+                },
+                getCurrentFilter: { self.currentFilter },
+                onStreamStop: {
+                    self.runningStream = nil
+                }
+            )
+            self.scDelegate = StreamToFramesDelegate(recorder: avRecorder, callbacks: callbacks)
         }
     }
 }
 
-//manage streaming data from SCKit, AV Video devices (camera, ipad), and audio devices
-// video frames will get sent to the continuation parameter
-// video + audio will get sent to the recorder if we're recording
-class StreamToFramesDelegate : NSObject, SCStreamDelegate, SCStreamOutput,
-                               AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate {
-    var logger = Logger()
-    
-    private var trigger = ConservativeTrigger()
-    private let ciContext = CIContext()
-    
-    private var streamDimensions = CGSize(width: 1920, height: 1080)
-    private var continuation: AsyncThrowingStream<FrameType, Error>.Continuation
-    private var recorder: AVRecorder
-    private var getCurrentFilter: () -> SCContentFilter?
-    private var onStreamStop: () -> () = {}
-    
-    init(continuation: AsyncThrowingStream<FrameType, Error>.Continuation,
-         recorder: AVRecorder,
-         getCurrentFilter: @escaping () -> SCContentFilter?,
-         onStreamStop: @escaping ()->()){
-        self.continuation = continuation
-        self.recorder = recorder
-        self.getCurrentFilter = getCurrentFilter
-        self.onStreamStop = onStreamStop
-        logger.info("Created stream delegate")
-    }
-    
-    func stream(_ stream: SCStream, didOutputSampleBuffer buffer: CMSampleBuffer, of: SCStreamOutputType){
-        guard buffer.isValid else {
-            logger.info("invalid buffer in stream")
-            return
-        }
-        //get the sample buffer attachments for some reason?
-        guard let attachmentsArray = CMSampleBufferGetSampleAttachmentsArray(buffer,
-                                                                             createIfNecessary: false) as? [[SCStreamFrameInfo: Any]],
-              let attachments = attachmentsArray.first else { return }
-        
-        // Validate the status of the frame. If it isn't `.complete`, return nil.
-        guard let statusRawValue = attachments[SCStreamFrameInfo.status] as? Int,
-              let status = SCFrameStatus(rawValue: statusRawValue),
-              status == .complete else {
-            logger.info("incomplete buffer in stream")
-            return
-        }
-        
-        guard let contentRectDict = attachments[.contentRect],
-              let contentScale = attachments[.contentScale] as? CGFloat,
-              let scaleFactor = attachments[.scaleFactor] as? CGFloat,
-              let contentRect = CGRect(dictionaryRepresentation: contentRectDict as! CFDictionary) else {
-            logger.error("Couldn't get contectRect!")
-            return
-        }
-        let scaledSize = CGSize(width: contentRect.size.width*scaleFactor, height: contentRect.size.height*scaleFactor)
-        let unscaledContentSize = CGSize(width: contentRect.size.width/contentScale, height: contentRect.size.height/contentScale)
-        
-        guard of == SCStreamOutputType.screen else { return }
-        
-        //extract the image
-        guard let pixelBuffer = buffer.imageBuffer else {
-            logger.info("couldn't get pixel buffer in stream")
-            return
-        }
-        
-        guard let surfaceRef = CVPixelBufferGetIOSurface(pixelBuffer)?.takeUnretainedValue() else {
-            logger.error("Couldn't get IOSurface in stream")
-            return
-        }
-       
-        let surface = unsafeBitCast(surfaceRef, to: IOSurface.self)
-        
-        let croppingRequired = !rectsApproxEqual(scaledSize, CGSize(width:surface.width, height: surface.height))
-        //if we don't need to crop then the size couldn't have changed
-        let sizeChanged = croppingRequired && self.streamDimensions != scaledSize
-        if(sizeChanged){
-            self.streamDimensions = scaledSize
-        }
-        
-        let triggered = trigger.tick(updateOccurred: sizeChanged)
-        if(triggered){
-            //update the stream config
-            Task {
-                do {
-                    //filter is not updated...
-                    logger.debug("updating config with dimensions: \(unscaledContentSize.debugDescription)")
-                    try await stream.updateConfiguration(getStreamConfig(unscaledContentSize))
-                    logger.debug("stream config updated")
-                } catch {
-                    logger.error("couldn't update stream: \(error)")
-                }
-            }
-        }
-        //after the config update happens we should be able to do this
-        if(!croppingRequired){
-            continuation.yield(FrameType.uncropped(surface))
-            if(recorder.recording){
-                //TODO mirror frame here, maybe
-                recorder.writeFrame(frame: pixelBuffer)
-            }
-            return
-        }
-        
-        //crop it to the content rect size
-        let cii = CIImage(ioSurface: surface)
-        //the CVPixelBuffer doesn't support the default format which is RGBA8, so specify that we want it as BGRA8 here
-        guard let cgImage =
-                ciContext.createCGImage(cii,
-                                        from: CGRect(origin: CGPoint(x: 0, y: surface.height - Int(scaledSize.height)),
-                                                     size: scaledSize),
-                                        format: .BGRA8,
-                                        colorSpace: CGColorSpace(name: CGColorSpace.sRGB)) else {
-            logger.error("Couldn't make CGImage")
-            return
-        }
-        continuation.yield(FrameType.cropped(cgImage))
-        if(recorder.recording){
-            let pb = pixelBufferFromCGImage(image: cgImage)
-            if(pb != nil){
-                recorder.writeFrame(frame: pb!)
-            } else {
-                logger.debug("CG to CVPixelBuf conversion failed")
-            }
-        }
-    }
-    
-    func stream(_ stream: SCStream, didStopWithError error: Error) {
-
-        logger.debug("STREAM STOPPED WITH ERROR")
-        let nserr: NSError = error as NSError
-        logger.debug("error code \(nserr.code)")
-        if(nserr.code == SCStreamError.systemStoppedStream.rawValue){
-            logger.debug("System stopped the erorr, restart it!")
-            //TODO store the filter and reenable the stream from it
-            let filter = getCurrentFilter()
-            if(filter != nil){
-                Task {
-                    do{
-                        try await stream.updateContentFilter(filter!)
-                    } catch {
-                        logger.error("couldn't restart the stream: \(error)")
-                    }
-                }
-            }
-            
-        } else {
-            //continuation.finish()
-            continuation.yield(FrameType.cropped(sharingStoppedImage))
-            if(recorder.recording){
-                recorder.writeFrame(frame: pixelBufferFromCGImage(image: sharingStoppedImage)!)
-            }
-            onStreamStop()
-        }
-        
-        
-    }
-    
-    // like stream methods above, but for AV devices
-    func captureOutput(_ output: AVCaptureOutput, didOutput buffer: CMSampleBuffer,
-                       from connection: AVCaptureConnection ){
-        guard buffer.isValid else {
-            logger.debug("invalid AV Buffer")
-            return
-        }
-        
-        if(buffer.formatDescription?.mediaType == .video){
-            
-            guard let pixelBuffer = buffer.imageBuffer else {
-                logger.debug("couldn't get AV Pixel buffer")
-                return
-            }
-            
-            guard let surfaceRef = CVPixelBufferGetIOSurface(pixelBuffer)?.takeUnretainedValue() else {
-                logger.error("Couldn't get IOSurface")
-                return
-            }
-            
-            let surface = unsafeBitCast(surfaceRef, to: IOSurface.self)
-            continuation.yield(FrameType.uncropped(surface))
-            if(recorder.recording){
-                //TODO mirror frame here, maybe
-                recorder.writeFrame(frame: pixelBuffer)
-            }
-        } else if(buffer.formatDescription?.mediaType == .audio){
-            if(recorder.recording){
-                recorder.writeAudioSample(buffer:buffer)
-            }
-        } else {
-            logger.debug("unknown format: \(buffer.formatDescription.debugDescription)")
-        }
-    }
-    
-    func captureOutput(_: AVCaptureOutput, didDrop: CMSampleBuffer, from: AVCaptureConnection){
-        logger.debug("AV frame dropped")
-    }
-}
-
-
-private func rectsApproxEqual(_ r1: CGSize, _ r2: CGSize) -> Bool{
+func rectsApproxEqual(_ r1: CGSize, _ r2: CGSize) -> Bool{
     return (abs(r1.width - r2.width) + abs(r1.height - r2.height)) < 5 //+/- ~ 2 pixels in each dimension seems fine
 }
 
 let FrameScaling = 2
 
-private func getStreamConfig(_ streamDimensions: CGSize) -> SCStreamConfiguration {
+func getStreamConfig(_ streamDimensions: CGSize) -> SCStreamConfiguration {
     let conf = SCStreamConfiguration()
     conf.capturesAudio = false
     conf.width = FrameScaling*Int(streamDimensions.width)
@@ -482,7 +290,7 @@ private func getCurrentlySharedWindow(size: CGSize) async -> [SCWindow] {
 
 
 //from https://stackoverflow.com/questions/38318387/swift-cgimage-to-cvpixelbuffer
-private func pixelBufferFromCGImage(image: CGImage) -> CVPixelBuffer? {
+func pixelBufferFromCGImage(image: CGImage) -> CVPixelBuffer? {
     
     guard let imageData = image.dataProvider?.data,
         let mutableData = CFDataCreateMutableCopy(
---
+++ presenterMode/StreamToFramesDelegate.swift
@@ -0,0 +1,206 @@
+//
+//  StreamToFramesDelegate.swift
+//  presenterMode
+//
+//  Created by Ben Jones on 6/13/24.
+//
+
+import AVFoundation
+import CoreImage
+import IOSurface
+import OSLog
+import ScreenCaptureKit
+
+struct StreamFrameCallbacks {
+    let onFrame: (FrameType) -> Void
+    let getCurrentFilter: () -> SCContentFilter?
+    let onStreamStop: () -> Void
+}
+
+// manage streaming data from SCKit, AV Video devices (camera, ipad), and audio devices
+// video frames will get sent to onFrame
+// video + audio will get sent to the recorder if we're recording
+class StreamToFramesDelegate: NSObject, SCStreamDelegate, SCStreamOutput,
+                              AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate {
+    var logger = Logger()
+    
+    private var trigger = ConservativeTrigger()
+    private let ciContext = CIContext()
+    
+    private var streamDimensions = CGSize(width: 1920, height: 1080)
+    private var recorder: AVRecorder
+    private var callbacks: StreamFrameCallbacks
+    
+    init(recorder: AVRecorder, callbacks: StreamFrameCallbacks) {
+        self.recorder = recorder
+        self.callbacks = callbacks
+        logger.info("Created stream delegate")
+    }
+    
+    func stream(_ stream: SCStream, didOutputSampleBuffer buffer: CMSampleBuffer, of: SCStreamOutputType) {
+        guard buffer.isValid else {
+            logger.info("invalid buffer in stream")
+            return
+        }
+        //get the sample buffer attachments for some reason?
+        guard let attachmentsArray = CMSampleBufferGetSampleAttachmentsArray(buffer,
+                                                                             createIfNecessary: false) as? [[SCStreamFrameInfo: Any]],
+              let attachments = attachmentsArray.first else { return }
+        
+        // Validate the status of the frame. If it isn't `.complete`, return nil.
+        guard let statusRawValue = attachments[SCStreamFrameInfo.status] as? Int,
+              let status = SCFrameStatus(rawValue: statusRawValue),
+              status == .complete else {
+            logger.info("incomplete buffer in stream")
+            return
+        }
+        
+        guard let contentRectDict = attachments[.contentRect],
+              let contentScale = attachments[.contentScale] as? CGFloat,
+              let scaleFactor = attachments[.scaleFactor] as? CGFloat,
+              let contentRect = CGRect(dictionaryRepresentation: contentRectDict as! CFDictionary) else {
+            logger.error("Couldn't get contectRect!")
+            return
+        }
+        let scaledSize = CGSize(width: contentRect.size.width*scaleFactor, height: contentRect.size.height*scaleFactor)
+        let unscaledContentSize = CGSize(width: contentRect.size.width/contentScale, height: contentRect.size.height/contentScale)
+        
+        guard of == SCStreamOutputType.screen else { return }
+        
+        //extract the image
+        guard let pixelBuffer = buffer.imageBuffer else {
+            logger.info("couldn't get pixel buffer in stream")
+            return
+        }
+        
+        guard let surfaceRef = CVPixelBufferGetIOSurface(pixelBuffer)?.takeUnretainedValue() else {
+            logger.error("Couldn't get IOSurface in stream")
+            return
+        }
+       
+        let surface = unsafeBitCast(surfaceRef, to: IOSurface.self)
+        
+        let croppingRequired = !rectsApproxEqual(scaledSize, CGSize(width:surface.width, height: surface.height))
+        //if we don't need to crop then the size couldn't have changed
+        let sizeChanged = croppingRequired && self.streamDimensions != scaledSize
+        if(sizeChanged){
+            self.streamDimensions = scaledSize
+        }
+        
+        let triggered = trigger.tick(updateOccurred: sizeChanged)
+        if(triggered){
+            //update the stream config
+            Task {
+                do {
+                    //filter is not updated...
+                    logger.debug("updating config with dimensions: \(unscaledContentSize.debugDescription)")
+                    try await stream.updateConfiguration(getStreamConfig(unscaledContentSize))
+                    logger.debug("stream config updated")
+                } catch {
+                    logger.error("couldn't update stream: \(error)")
+                }
+            }
+        }
+        //after the config update happens we should be able to do this
+        if(!croppingRequired){
+            callbacks.onFrame(FrameType.uncropped(surface))
+            if(recorder.recording){
+                //TODO mirror frame here, maybe
+                recorder.writeFrame(frame: pixelBuffer)
+            }
+            return
+        }
+        
+        //crop it to the content rect size
+        let cii = CIImage(ioSurface: surface)
+        //the CVPixelBuffer doesn't support the default format which is RGBA8, so specify that we want it as BGRA8 here
+        guard let cgImage =
+                ciContext.createCGImage(cii,
+                                        from: CGRect(origin: CGPoint(x: 0, y: surface.height - Int(scaledSize.height)),
+                                                     size: scaledSize),
+                                        format: .BGRA8,
+                                        colorSpace: CGColorSpace(name: CGColorSpace.sRGB)) else {
+            logger.error("Couldn't make CGImage")
+            return
+        }
+        callbacks.onFrame(FrameType.cropped(cgImage))
+        if(recorder.recording){
+            let pb = pixelBufferFromCGImage(image: cgImage)
+            if(pb != nil){
+                recorder.writeFrame(frame: pb!)
+            } else {
+                logger.debug("CG to CVPixelBuf conversion failed")
+            }
+        }
+    }
+    
+    func stream(_ stream: SCStream, didStopWithError error: Error) {
+
+        logger.debug("STREAM STOPPED WITH ERROR")
+        let nserr: NSError = error as NSError
+        logger.debug("error code \(nserr.code)")
+        if(nserr.code == SCStreamError.systemStoppedStream.rawValue){
+            logger.debug("System stopped the erorr, restart it!")
+            //TODO store the filter and reenable the stream from it
+            let filter = callbacks.getCurrentFilter()
+            if(filter != nil){
+                Task {
+                    do{
+                        try await stream.updateContentFilter(filter!)
+                    } catch {
+                        logger.error("couldn't restart the stream: \(error)")
+                    }
+                }
+            }
+            
+        } else {
+            //continuation.finish()
+            callbacks.onFrame(FrameType.cropped(sharingStoppedImage))
+            if(recorder.recording){
+                recorder.writeFrame(frame: pixelBufferFromCGImage(image: sharingStoppedImage)!)
+            }
+            callbacks.onStreamStop()
+        }
+        
+        
+    }
+    
+    // like stream methods above, but for AV devices
+    func captureOutput(_ output: AVCaptureOutput, didOutput buffer: CMSampleBuffer,
+                       from connection: AVCaptureConnection ){
+        guard buffer.isValid else {
+            logger.debug("invalid AV Buffer")
+            return
+        }
+        
+        if(buffer.formatDescription?.mediaType == .video){
+            
+            guard let pixelBuffer = buffer.imageBuffer else {
+                logger.debug("couldn't get AV Pixel buffer")
+                return
+            }
+            
+            guard let surfaceRef = CVPixelBufferGetIOSurface(pixelBuffer)?.takeUnretainedValue() else {
+                logger.error("Couldn't get IOSurface")
+                return
+            }
+            
+            let surface = unsafeBitCast(surfaceRef, to: IOSurface.self)
+            callbacks.onFrame(FrameType.uncropped(surface))
+            if(recorder.recording){
+                //TODO mirror frame here, maybe
+                recorder.writeFrame(frame: pixelBuffer)
+            }
+        } else if(buffer.formatDescription?.mediaType == .audio){
+            if(recorder.recording){
+                recorder.writeAudioSample(buffer:buffer)
+            }
+        } else {
+            logger.debug("unknown format: \(buffer.formatDescription.debugDescription)")
+        }
+    }
+    
+    func captureOutput(_: AVCaptureOutput, didDrop: CMSampleBuffer, from: AVCaptureConnection){
+        logger.debug("AV frame dropped")
+    }
+}
